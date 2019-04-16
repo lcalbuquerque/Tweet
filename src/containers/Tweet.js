4ca98923679100d64004bf5ca887faa1f1d06fde
Luis Albuquerque <luis.albuq.oliveira@gmail.com>
2019-04-16 15:26:02 -0300

    Versão Final - Tweet

--- src/containers/Tweet.js
+++ src/containers/Tweet.js
@@ -28,13 +28,11 @@ class Tweet extends Component {
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
     const tweet = tweets[id];
-    const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
+    const parentTweet = tweets[tweet.replyingTo];
 
     return {
         authedUser,
-        tweet: tweet ?
-            formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
-            : null
+        tweet: formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
     };
 }
 
